2126ced29bb898b5a704bfbdf413e6e61d99fd7b
jodafons <jodafons@lps.ufrj.br>
2019-04-04 05:26:54 +0200

    isolate prometheus and Gaugi.

--- python/__init__.py
+++ python/__init__.py
@@ -17,26 +17,6 @@ from . import StatusCode
 __all__.extend(StatusCode.__all__)
 from .StatusCode import *
 
-from . import Algorithm
-__all__.extend(Algorithm.__all__)
-from .Algorithm import *
-
-from . import Service
-__all__.extend(Service.__all__)
-from .Service import *
-
-from . import EventSimulator
-__all__.extend(EventSimulator.__all__)
-from .EventSimulator import *
-
-from . import EventATLAS
-__all__.extend(EventATLAS.__all__)
-from .EventATLAS import *
-
-from . import EventContext
-__all__.extend(EventContext.__all__)
-from .EventContext import *
-
 from . import enumerations
 __all__.extend(enumerations.__all__)
 from .enumerations import *
@@ -45,9 +25,9 @@ from . import utilities
 __all__.extend(utilities.__all__)
 from .utilities import *
 
-from . import mainloop
-__all__.extend(mainloop.__all__)
-from .mainloop import *
+from . import parallel
+__all__.extend(parallel.__all__)
+from .parallel import *
 
 from . import constants
 __all__.extend(constants.__all__)
