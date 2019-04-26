8dfc692abad28d48bae2a7b54ef19e9443dde412
jodafons <jodafons@lps.ufrj.br>
2019-04-26 21:27:45 +0200

    add cmake gaugi scripts

--- cmake/InstallGaugiFunctions.cmake
+++ cmake/InstallGaugiFunctions.cmake
@@ -1,9 +1,9 @@
 
 
-function(gaugi_build_directories)
+function(gaugi_initialize)
   file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/python)
   file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
-endfunction(gaugi_build_directories)
+endfunction(gaugi_initialize)
 
 
 function(gaugi_install_python_modules filepath module)
@@ -12,3 +12,12 @@ function(gaugi_install_python_modules filepath module)
 endfunction(gaugi_install_python_modules)
 
 
+
+function( gaugi_finalize )
+
+  file(GLOB SO_FILES ${CMAKE_BINARY_DIR}/*.so)
+  FOREACH( SFILE ${SO_FILES})
+    execute_process(POST_BUILD COMMAND ${CMAKE_COMMAND} -E create_symlink ${SFILE} ${CMAKE_BINARY_DIR}/lib/)
+  ENDFOREACH()
+
+endfunction( gaugi_finalize )
