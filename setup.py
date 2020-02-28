c02cb1a5d5026611b343dfcef4ab58cb480c57e1
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2020-02-28 12:06:45 -0300

    Reverting stuff

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name = 'Gaugi',
-  version = '2.0.0',
+  version = '2.0.4',
   license='GPL-3.0',
   description = '',
   long_description = long_description,
